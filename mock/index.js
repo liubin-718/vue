889a26eb70b5d464485cda88c84292d311ea00aa
liubin-718 <liubin718@163.com>
2020-05-09 18:05:00 +0800

    add mock数据

--- mock/index.js
+++ mock/index.js
@@ -1,3 +1,42 @@
 import Mock from 'mockjs'
 
 import electronicBill from './electronicBill'
+
+const mocks = [
+    ...electronicBill
+]
+
+export function mockXHR(){
+    Mock.XHR.prototype.proxy_send = Mock.XHR.prototype.send
+    Mock.XHR.prototype.send = function(){
+        if(this.custom.xhr){
+            this.custom.xhr.withCredentials = this.withCredentials || false
+            if(this.responseType){
+                this.custom.xhr.responseType = this.responseType
+            }
+        }
+        this.proxy_send(...arguments)
+    }
+
+    function XHR2ExpressReqWrap(respond){
+        return function(options){
+            let result = null
+            if(respond instanceof Function){
+                const {body, type, url} = options
+                result = respond({
+                    method: type,
+                    body: JSON.parse(body)
+                })
+            }else{
+                result = respond
+            }
+            return Mock.mock(result)
+        }
+    }
+
+    for(const i of mocks){
+        Mock.mock(i.url, i.type || 'get', XHR2ExpressReqWrap(i.response))
+    }
+}
+
+export default mocks
